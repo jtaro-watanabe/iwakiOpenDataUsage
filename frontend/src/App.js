8f7e6bc71ce5d9560f8a0e76414104888ffa97b9
jtaro <jtaro.watanabe@gmail.com>
2022-01-27 21:15:03 +0900

    市場取扱高グラフ画面追加

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import ButtonAppBar from './molecules/ButtonAppBar';
 import PopulationChart from './organisms/PopulationChart';
+import MarketTransactionQuantityChart from './organisms/MarketTransactionQuantityChart';
 
 function App() {
   return (
     <div className="App">
        <ButtonAppBar/> 
-       <PopulationChart />
+       <MarketTransactionQuantityChart />
     </div>
   );
 }
